f77934ae702426ec91d1dea4839c240014b68052
AVS-2307 <andrey230781.an@gmail.com>
2023-01-27 19:43:02 +0300

    Реализация через функции

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
-/* function getRandomIntInclusive(min, max) {
+// задаём число
+function getRandomIntInclusive(min, max) {
   min = Math.ceil(min)
   max = Math.floor(max)
   let secretNumber = Math.floor(Math.random() * (max - min + 1)) + min
@@ -6,28 +7,46 @@
   return secretNumber //Максимум и минимум включаются
 }
 
+// запрашиваем число от пользователя
 function getUserNumber() {
-    let userNumber = document.getElementById('userNumber').value;        
-    document.getElementById('userNumber').innerHTML = userNumber;
+    let userNumber = prompt("Введите число от 0 до 999:")
     console.log(`Введено число ${userNumber}`);         
     return userNumber        
 } 
 
-let secretNumber = getRandomIntInclusive(0, 999)
-let userNumber = getUserNumber()
+// сравниваем результаты
+function checkResult () {
+    while(true) {
+        let secretNumber =  getRandomIntInclusive(0, 999)
+        let userNumber = getUserNumber()
+            
+        if (userNumber === 'q') {
+            alert('Спасибо за участие!')
+            return
+        }
+        
+        if (isNaN(userNumber) || +userNumber < 0 || +userNumber > 999) {
+            alert("Введите, пожалуйста, ЧИСЛО от 0 до 999:")        
+        }
 
-function checkResult (secretNumber, userNumber) {
-    if (secretNumber === userNumber) {
-        alert("Вы угадали!");
+        else if (secretNumber === +userNumber)    {
+            return alert("Вы угадали!")
+        }
+
+        else if (+userNumber >= secretNumber) {
+            alert("Введенное число больше загаданного, попробуйте ещё раз")
+        }
+        else if (+userNumber <= secretNumber) {
+            alert("Введенное число меньше загаданного, попробуйте ещё раз")
+        }
     }    
+    
 }
 
-getRandomIntInclusive(0, 999)
-getUserNumber()
-checkResult() */
+checkResult() 
 
 
-while(true) {
+/*while(true) {
     let secretNumber = Math.floor(Math.random() * (999 - 0 + 1)) + 0
     console.log(`Загадано ${secretNumber}`)
 
@@ -53,4 +72,4 @@ while(true) {
         alert("Введенное число меньше загаданного, попробуйте ещё раз")
     }
 
-}
+}*/
